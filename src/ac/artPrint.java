212fec3b01d0d64d2e12f60d721291610b989f75
Art Cohen <agitatorsband@gmail.com>
2017-12-03 12:42:42 -0500

    Refactor Print command and set up Day Three

---
+++ src/ac/artPrint.java
@@ -0,0 +1,11 @@
+package ac;
+
+public class artPrint {
+    public static void print(String string) {
+        System.out.println(string);
+    }
+
+    public static void print(int string) {
+        print(Integer.toString(string));
+    }
+}
