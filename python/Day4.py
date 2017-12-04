858cb4711e58e5465be5dcaaf83a94bd53ead775
Art Cohen <art.cohen@ipreo.com>
2017-12-04 13:54:31 -0500

    Day 4 Phase 2 (could probably use cleanup)

--- python/Day4.py
+++ python/Day4.py
@@ -8,35 +8,59 @@ f.close()
 lines = message.split("\n");
 
 i = 0
-for line in lines:
-	if len(line) == 0:
-		next
-		
-	# print(str(i))
-	lineIsGood = True;
-	
-	
-	words = line.split()
-	
-	wordCount = dict()
-	if (len(words)== 0):
-		lineIsGood = False
-	else :
-		for word in words:
-			# print(word)
-			
-			if word in wordCount:
-				lineIsGood = False;
-				# print (line)
-				break;
-			else:
-				
-				wordCount[word] = 1;
-	
-	
-	if lineIsGood:
-		# print ("'" + line + "'")
-		i += 1;
+i2 = 0
 
-		
+
+def sortWord(word):
+    wordRA = list(word)
+    wordRA.sort()
+    # return "".join(wordRA)
+    print (word)
+    print (wordRA)
+    return "".join(wordRA)
+
+
+for line in lines:
+    if len(line) == 0:
+        next
+
+    # print(str(i))
+    lineIsGood = True;
+    lineIsGood2 = True;
+
+
+    words = line.split()
+
+    wordCount = dict()
+    wordCount2 = dict()
+    if (len(words)== 0):
+        lineIsGood = False
+        lineIsGood2 = False
+    else :
+        for word in words:
+            # print(word)
+
+            # PHASE ONE:
+            if word in wordCount:
+                lineIsGood = False
+                # print (line)
+                # break;
+            else:
+                wordCount[word] = 1
+
+            wordsorted = sortWord(word)
+            if wordsorted in wordCount2:
+                lineIsGood2 = False
+            else:
+                wordCount2[wordsorted] = 1
+
+
+    if lineIsGood:
+        # print ("'" + line + "'")
+        i += 1
+
+    if lineIsGood2:
+        i2 += 1
+
 print (str(i) + " valid passphrases")
+print (str(i2) + " valid passphrases with anagrams")
