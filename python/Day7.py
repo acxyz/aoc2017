4cb976acb03c3607d11b3c1997d41aa062f28f9e
Art Cohen <agitatorsband@gmail.com>
2017-12-07 13:07:21 -0500

    cleanup

--- python/Day7.py
+++ python/Day7.py
@@ -20,9 +20,6 @@ for line in lines:
 	print(line)
 	matches = rgx_line.match(line)
 	if matches:
-		# print ("match")
-		# print(matches.group())
-		# print (matches.groups())
 		name = matches.group('name')
 		weight = matches.group('weight')
 		print (" - " + name + " [" + weight + "]")
@@ -38,60 +35,3 @@ for line in lines:
 	else:
 		print ("nomatch")
 			
-
-# for bank in banks: 
-	# print(bank)
-
-# def getMax (banks):
-	# max_value = max(banks)
-	# max_index = banks.index(max_value)
-	# print (max_value)
-	# print (max_index)
-	# return (max_index, max_value)
-	
-# print("----------------------")
-
-# print(banks)
-
-# def redistributeMemory(banks):
-	# (curr_index, num_to_distribute) = getMax(banks)
-	# banks[curr_index] = 0
-	# while num_to_distribute > 0:
-		# curr_index += 1
-		# if (curr_index) >= len(banks):
-			# curr_index = 0
-		# banks[curr_index] += 1
-		# num_to_distribute -= 1
-
-
-# iterations = 0
-
-# states = set()
-# statesDict = {} # phase 2
-# states.add(tuple(banks))
-# statesDict[tuple(banks)] = iterations
-
-
-# while (True):
-	# print ("Size is " + str(len(states)))
-	# redistributeMemory(banks)
-	# iterations += 1
-	# print (banks)
-	# if tuple(banks) in states:
-		# break
-	
-	# states.add(tuple(banks))
-	# statesDict[tuple(banks)] = iterations
-	
-
-# print (str(iterations) + " iterations")
-# print (str(iterations - statesDict[tuple(banks)]) + " cycles since duple first appeared"	)
-# # print (foo in states)
-# # print(banks)
-# # states.add(tuple(banks))
-# # print ("Size is " + str(len(states)))
-# # redistributeMemory(banks)
-# # print(banks)
-# # states.add(tuple(banks))
-# # print ("Size is " + str(len(states)))
-# # print (foo in states)
