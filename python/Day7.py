e3544a79aa3e678671fbc46033ef880bae55f65d
Art Cohen <agitatorsband@gmail.com>
2017-12-07 13:04:07 -0500

    regex redux

--- python/Day7.py
+++ python/Day7.py
@@ -12,13 +12,29 @@ lines = message.split("\n")
 # ebmcu (30) -> glsrg, ckhip, rqhhyc, jjvxxtt, rdnpoms
 # wuybunc (54)
 
-rgx = re.compile(r"^(\w+) \((\d+)\) ->(( \w+),?)+")
+# used named group:
+rgx_line = re.compile(r"^(?P<name>\w+) \((?P<weight>\d+)\)( ->(?P<branches>( \w+\,?)+))?" )
+rgx_branches = re.compile(r"\w+")
 
 for line in lines:
-	m = rgx.match(line)
-	if m:
-		print ("match")
-		print(m.group())
+	print(line)
+	matches = rgx_line.match(line)
+	if matches:
+		# print ("match")
+		# print(matches.group())
+		# print (matches.groups())
+		name = matches.group('name')
+		weight = matches.group('weight')
+		print (" - " + name + " [" + weight + "]")
+		
+		branches = matches.group('branches')
+		if branches:
+			
+			l = rgx_branches.findall(branches)
+			
+			for branch in l: 
+				print(" -- " + branch)
+				
 	else:
 		print ("nomatch")
 			
