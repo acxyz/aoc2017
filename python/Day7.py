063a4c677affa43d33a79cc62e0ce78dda5f289c
Art Cohen <agitatorsband@gmail.com>
2017-12-09 13:54:45 -0500

    Day 7 final

--- python/Day7.py
+++ python/Day7.py
@@ -3,8 +3,8 @@ import sys
 
 class disc:
 
-	weight = 0
-	name = ""
+	# weight = 0
+	# name = ""
 	
 	def __init__(self, name, weight):
 		self.name = name
@@ -34,7 +34,7 @@ rgx_line = re.compile(r"^(?P<name>\w+) \((?P<weight>\d+)\)( ->(?P<branches>( \w+
 rgx_branches = re.compile(r"\w+")
 
 for line in lines:
-	print(line)
+	# print(line)
 	matches = rgx_line.match(line)
 	if matches:
 		name = matches.group('name')
@@ -49,9 +49,9 @@ for line in lines:
 			l = rgx_branches.findall(branches)
 			
 			for branch in l: 
-				print(" -- " + branch)
+				# print(" -- " + branch)
 				d.children.append(branch)
-			print (d.children)
+			# print (d.children)
 		discs[name] = d
 	
 	else:
@@ -61,10 +61,10 @@ for line in lines:
 # print (discs)
 
 for disc_name, disc in discs.items(): 
-	print (disc_name)
-	print(disc.children)
+	# print (disc_name)
+	# print(disc.children)
 	for c in disc.children:
-		print (c + '-' + disc_name)
+		# print (c + '-' + disc_name)
 		discs[c].parent = disc_name	
 
 for disc_name, disc in discs.items():
@@ -72,22 +72,87 @@ for disc_name, disc in discs.items():
 		ult_parent = disc
 		print("ultimate parent is " + disc.name)
 
-for disc_name, disc in discs.items():
-	print (disc_name + " <- " + disc.parent if disc.parent != None else "")
+# for disc_name, disc in discs.items():
+	# print (disc_name + " <- " + (disc.parent if disc.parent != None else ""))
+	# for kid in disc.children:
+		# print (" - " + kid)
 
+def printWithKids(disc, indent):
+	
+	for i in range(indent):
+		print ("\t", end="")
+	print (disc.name + " [" + str(disc.weight) + "]")
+	for c in disc.children:
+		printWithKids(discs[c], indent + 1)
+	# print("")
+
+printWithKids(ult_parent,0)		
+	
+		
 def calcWeight(disc):
 	tot_weight = disc.weight
 	# print ("tot weight is " + str(tot_weight))
 	# print (disc.children)
 	for kid in disc.children:
 		kid_disc = discs[kid]
-		if kid_disc.parent == disc:
-			tot_weight += calcWeight(discs[kid])
+		tot_weight += calcWeight(discs[kid])
 	return tot_weight
 
-ult_weight = calcWeight(ult_parent)
+# ult_weight = calcWeight(ult_parent)
 
-print(ult_weight)
+# print(ult_weight)
+
+def findBad(disc):
+	weights = {}		
+	
+	# weigh each child
+	for c in disc.children:
+		kid = discs[c]
 		
+		w = calcWeight(kid)
+		kid.totalWeight = w
+		print (kid.name + " weighs " + str(w))
+		# print(w)
+		if w in weights:
+			weights[w].append(kid)
+		else:
+			weights[w] = []
+			weights[w].append(kid)
+
+	badDisc = None
+	badWeight = 0
+	for weight, weightList in weights.items():
+		if len(weightList) == 1:
+			badDisc = weightList[0]
+			badWeight = weight
+			print ("bad weight is " + str(badWeight))
+			
+		else:
+			goodWeight = weight
+
+	print(badWeight - goodWeight)
+	if (badDisc != None):
+		badDisc.excessWeight = badWeight - goodWeight
+	return (badDisc)
+	
+	
+	# if here, there are no unbalanced discs on top of this one.
+	return None
+			
+# (bad , weight) = findBad(ult_parent)
+# print (bad.name)			
 		
+def crawlTree(disc):
+	bd = findBad(disc)
+	if (bd == None):
+		# this is the bad disc
+		print (disc.name)
+		print (" the excessWeight weight is " + str(disc.excessWeight))
+		print (" the proper weight is " + str(disc.weight - disc.excessWeight))
+	else:
+		crawlTree(bd)
+		
+crawlTree (ult_parent)
+
+	
 
